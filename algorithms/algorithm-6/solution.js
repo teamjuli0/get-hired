927f24fb3e6403953886bafe5c3be0aa6e090afd
Julio Valdez <valdezjulio95@gmail.com>
2021-03-23 20:27:53 -0700

    made solution a bit drier

--- algorithms/algorithm-6/solution.js
+++ algorithms/algorithm-6/solution.js
@@ -1,40 +1,29 @@
-const addTwoNumbers = function (l1, l2) {
+const addTwoNumbers = (l1, l2) => {
+  const addToArr = (num, arr) => {
+    if (num.length === 1) {
+      remainder = 0
+      arr.push(Number(num))
+    } else {
+      remainder = Number(num[0])
+      arr.push(Number(num[1]))
+    }
+  }
+
   let remainder = 0
-  let totalLength = l1.length > l2.length ? l1.length : l2.length
+  let longest = l1.length > l2.length ? l1.length : l2.length
 
   const newArr = []
 
-  for (let i = 0; i < totalLength; i++) {
+  for (let i = 0; i < longest; i++) {
     if (l1[i] !== undefined && l2[i] !== undefined) {
       let newNum = (l1[i] + l2[i] + remainder).toString()
-
-      if (newNum.length === 1) {
-        remainder = 0
-        newArr.push(Number(newNum))
-      } else {
-        remainder = Number(newNum[0])
-        newArr.push(Number(newNum[1]))
-      }
+      addToArr(newNum, newArr)
     } else if (l1[i] === undefined) {
       let newNum = (l2[i] + remainder).toString()
-
-      if (newNum.length === 1) {
-        remainder = 0
-        newArr.push(Number(newNum))
-      } else {
-        remainder = Number(newNum[0])
-        newArr.push(Number(newNum[1]))
-      }
+      addToArr(newNum, newArr)
     } else if (l2[i] === undefined) {
       let newNum = (l1[i] + remainder).toString()
-
-      if (newNum.length === 1) {
-        remainder = 0
-        newArr.push(Number(newNum))
-      } else {
-        remainder = Number(newNum[0])
-        newArr.push(Number(newNum[1]))
-      }
+      addToArr(newNum, newArr)
     }
   }
   newArr === [] ? newArr.push(Number(0)) : null
