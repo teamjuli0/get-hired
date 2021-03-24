142bb7ed52bace02dda9343dc606439a289bce5d
Julio Valdez <valdezjulio95@gmail.com>
2021-03-23 20:41:55 -0700

    retracted solution as solution was not a linked list but instead was an array

--- algorithms/algorithm-6/solution.js
+++ algorithms/algorithm-6/solution.js
@@ -1,36 +1,3 @@
-const addTwoNumbers = (l1, l2) => {
-  const addToArr = (num, arr) => {
-    if (num.length === 1) {
-      remainder = 0
-      arr.push(Number(num))
-    } else {
-      remainder = Number(num[0])
-      arr.push(Number(num[1]))
-    }
-  }
-
-  let remainder = 0
-  let longest = l1.length > l2.length ? l1.length : l2.length
-
-  const newArr = []
-
-  for (let i = 0; i < longest; i++) {
-    if (l1[i] !== undefined && l2[i] !== undefined) {
-      let newNum = (l1[i] + l2[i] + remainder).toString()
-      addToArr(newNum, newArr)
-    } else if (l1[i] === undefined) {
-      let newNum = (l2[i] + remainder).toString()
-      addToArr(newNum, newArr)
-    } else if (l2[i] === undefined) {
-      let newNum = (l1[i] + remainder).toString()
-      addToArr(newNum, newArr)
-    }
-  }
-  newArr === [] ? newArr.push(Number(0)) : null
-
-  remainder === 0 ? null : newArr.push(remainder)
-
-  return newArr
-}
+const addTwoNumbers = (l1, l2) => {}
 
 console.log(addTwoNumbers([7, 4, 8, 5, 6], [9, 5, 8, 6, 7]))
