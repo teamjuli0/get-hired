901219aee32b1c1bf34535da2de4f1d4e17b0945
Julio Valdez <valdezjulio95@gmail.com>
2021-03-30 23:02:47 -0700

    first try algorithm #7

--- algorithms/algorithm-7/solution.js
+++ algorithms/algorithm-7/solution.js
@@ -1,1 +1,35 @@
-const lengthOfLongestSubstring = (s) => {}
+// ===============================================================
+// Unfinished Algorithm
+// ===============================================================
+
+const subStr = 'abcabcde'
+
+const lengthOfLongestSubstring = (s) => {
+  const splitString = s.split('')
+  let longestStrLength = 0
+  let currentStrLength = 0
+  let currentSubStr = []
+  for (let i = 0; i < splitString.length; i++) {
+    if (i === 0) {
+      currentSubStr.push(splitString[i])
+      currentStrLength++
+    }
+    for (let j = i; j < splitString.length; j++) {
+      //   console.log(currentSubStr.indexOf(splitString[j + 1]))
+      if (currentSubStr.indexOf(splitString[j + 1]) === -1) {
+        currentSubStr.push(splitString[j + 1])
+        currentStrLength++
+      } else {
+        if (currentStrLength > longestStrLength) {
+          longestStrLength = currentStrLength
+        }
+        currentStrLength = 0
+        break
+      }
+    }
+  }
+
+  return longestStrLength
+}
+
+console.log(lengthOfLongestSubstring(subStr))
